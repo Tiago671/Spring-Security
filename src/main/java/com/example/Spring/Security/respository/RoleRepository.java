4785f62ca6dcf79a727b2429f33805cae0621512
Tiago860 <tiagocontatos2006@gmail.com>
2025-09-30 18:52:59 -0300

    Repository User and Role

---
+++ src/main/java/com/example/Spring/Security/respository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.example.Spring.Security.respository;
+
+import com.example.Spring.Security.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
